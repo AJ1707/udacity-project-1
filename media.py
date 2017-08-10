24171cddd47edc02a553b5cdea835169421c12b3
Arun kumar s <arunkumar98.ak95@gmail.com>
2017-08-10 05:08:12 -0700

    Update media.py

--- media.py
+++ media.py
@@ -1,6 +1,18 @@
+"""
+module displaying movie objects,attributes and instances
+"""
 import webbrowser
 
+
 class Movie():
+    """
+    class object store movie related information
+    """
+    
+    """
+    Initialize instance of class Movie
+    containing all the elements of the movie trailer
+    """
 
     def __init__(self,movie_title,movie_storyline,poster_image_url,trailer_youtube):
         self.title=movie_title
@@ -8,5 +20,8 @@ class Movie():
         self.poster_image_url=poster_image_url
         self.trailer_youtube_url=trailer_youtube
     def show_trailer(self):
+        """
+        initialize the instance to open the youtube url
+        """
         webbrowser.open(self.trailer_youtube_url)
 
